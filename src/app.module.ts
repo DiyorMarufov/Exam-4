0d70e572db564056786b83af7562165525e8d6df
Fendoxxin1 <fehdoxxindehlaviy@gmail.com>
2025-05-28 15:54:49 +0500

    admin va user yaratildi

---
+++ src/app.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { AdminModule } from './admin/admin.module';
+
+@Module({
+  imports: [UsersModule, AdminModule],
+})
+export class AppModule {}
